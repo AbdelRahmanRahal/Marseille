4cacaf37bc436eb4dc51ef7be960b8036b9de708
AbdelRahman <abdelrahman.rahalll@gmail.com>
2023-03-06 01:43:55 +0200

    Basic first prototype for setup file
    (and hopefully the only one)

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Voice assistant bot in the works'
+LONG_DESCRIPTION = 'Marseille, a voice assistant bot that can help you with various tasks and answer various questions'
+
+setup(
+    name = "Marseille",
+    version = VERSION,
+    author = "AbdelRahman Rahal",
+    # author_email="<mail@neuralnine.com>",
+    description = DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    long_description = long_description,
+    packages = find_packages(),
+    install_requires = ['speechrecognition', 'pyttsx3', 'tkinter'],
+    keywords = ['python', 'Marseille', 'voice assistant', 'bot', 'voice assistant bot', 'ai', 'speech recognition'],
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

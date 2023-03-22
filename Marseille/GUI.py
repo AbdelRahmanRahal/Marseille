27133c5c4bca679a3e9ceaaa7d9505a9598aab6d
AbdelRahman <abdelrahman.rahalll@gmail.com>
2023-03-22 10:25:51 +0200

    FINALLY I GOT IT

--- Marseille/GUI.py
+++ Marseille/GUI.py
@@ -5,12 +5,14 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setWindowTitle("Marseille")
+		self.button1 = SendButton()
+		self.setCentralWidget(self.button1)
 		# button = QPushButton("A button of sorts (press me)")
 		# self.setCentralWidget(button)
 
 class SendButton(QPushButton):
 	def __init__(self):
 		super().__init__()
-		self.QPushButton("A peculiar button...")
+		self.setText("A peculiar button...")
 		# self.setText("A peculiar button")
-		MainWindow.setCentralWidget(SendButton)
+		
